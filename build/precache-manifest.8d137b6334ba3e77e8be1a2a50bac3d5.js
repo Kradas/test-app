4973b55fc0b60f214aba9b9cb78c39b3a49fbfc6
Dmitry Kraev <kraevds@gmail.com>
2019-08-06 13:53:13 +0300

    build fix

--- build/precache-manifest.8d137b6334ba3e77e8be1a2a50bac3d5.js
+++ build/precache-manifest.8d137b6334ba3e77e8be1a2a50bac3d5.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "417940116411f21b7325272807a0fd73",
+    "revision": "5ab2db81c1900197bdf1746c03c45788",
     "url": "./index.html"
   },
   {
-    "revision": "cb5c7203a0cc80c16c7e",
+    "revision": "68dcbc4d1bb72c591aed",
     "url": "./static/css/main.f5a6c8cd.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/js/2.4cbb3087.chunk.js"
   },
   {
-    "revision": "cb5c7203a0cc80c16c7e",
-    "url": "./static/js/main.815183a2.chunk.js"
+    "revision": "68dcbc4d1bb72c591aed",
+    "url": "./static/js/main.ef30b802.chunk.js"
   },
   {
     "revision": "8c97409f0ee389fe75da",
