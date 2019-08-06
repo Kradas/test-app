6cd1cebf5e580e16892e1233ef0f3dec10826bbc
Dmitry Kraev <kraevds@gmail.com>
2019-08-06 13:03:08 +0300

    build rid

---
+++ build/precache-manifest.dd0591c6173ed84d9f241413f2242e96.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ebe3bb4697fdd3db26e353e2d1eb1d4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8c673b2da551e2af0e98",
+    "url": "./static/css/main.f5a6c8cd.chunk.css"
+  },
+  {
+    "revision": "18dec6efeb34f3ea9ae8",
+    "url": "./static/js/2.4cbb3087.chunk.js"
+  },
+  {
+    "revision": "8c673b2da551e2af0e98",
+    "url": "./static/js/main.8034ac99.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
