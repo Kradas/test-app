70c3593f2d3260429cf26a8fe6ad9da4b696a700
Dmitry Kraev <kraevds@gmail.com>
2019-08-06 14:00:27 +0300

    build fix

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.012ba6070c3b1b4888a38b996a51cb4d.js"
+  "./precache-manifest.8d137b6334ba3e77e8be1a2a50bac3d5.js"
 );
 
 self.addEventListener('message', (event) => {
